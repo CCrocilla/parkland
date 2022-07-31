719b76071158fd6b673adaaf63884a631b7abc3e
CCrocilla <cla.crocilla@gmail.com>
2022-07-31 02:26:16 +0000

    Add Edit Profile and Change Password Page

--- dashboard/forms.py
+++ dashboard/forms.py
@@ -1,15 +1,58 @@
 """ Imports """
 from django import forms
+from django.forms import ModelForm
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserChangeForm, PasswordChangeForm
 from car_park.models import Feedback
 
 
-class FeedbackForm(forms.ModelForm):
+CHOICES = [
+    (1, 'Bad'), (2, 'Poor'), (3, 'OK'), (4, 'Good'), (5, 'Excellent'), ]
+
+
+class FeedbackForm(ModelForm):
     """ Form for Users' Feedback """
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['created_by'].disabled = True
+    
     class Meta:
         model = Feedback
-        fields = ('title', 'comment')
+        fields = ('created_by', 'title', 'comment', 'rating_stars')
+        labels = {
+            'created_by': '',
+            'title': 'Title',
+            'comment': 'Comment',
+            'rating_stars': '',
+        }
 
         widgets = {
+            'created_by': forms.HiddenInput(),
             'title': forms.TextInput(attrs={'class': 'form-control'}),
-            'comment': forms.Textarea(attrs={'class': 'form-control'})
+            'comment': forms.Textarea(attrs={'class': 'form-control'}),
+            'rating_stars': forms.HiddenInput(),
+        }
+
+
+class EditProfileForm(UserChangeForm):
+    """ Form for User's Profile Edit Page """
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'last_name', 'email', 'date_joined', 'last_login')
+        labels = {
+            'username': 'Username',
+            'first_name': 'Title',
+            'last_name': 'Comment',
+            'email': 'Email',
+            'date_joined': 'Date Joined',
+            'last_login': 'Last Login',
+        }
+
+        widgets = {
+            'username': forms.TextInput(attrs={'class': 'form-control'}),
+            'first_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'last_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control'}),
+            'date_joined': forms.TextInput(attrs={'class': 'form-control', 'disabled': True}),
+            'last_login': forms.TextInput(attrs={'class': 'form-control', 'disabled': True}),
         }
--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,12 +1,18 @@
-from django.urls import path
-from .views import DashboardView, AddFeedbackView, ListFeedbackView, DetailsFeedbackView, EditFeedbackView, DeleteFeedbackView, EditUserView
+from django.urls import path, include
+from django.contrib.auth import views
+from .views import DashboardView, AddFeedbackView, ListFeedbackView, DetailsFeedbackView, EditFeedbackView, DeleteFeedbackView, EditProfileView, PasswordChangeProfileView, PasswordSuccess
+
 
 urlpatterns = [
     path('', DashboardView.as_view(), name='dashboard'),
+    path('', include('django.contrib.auth.urls')),
     path('feedback/add', AddFeedbackView.as_view(), name='add-feedback'),
     path('feedback/list', ListFeedbackView.as_view(), name='list-feedback'),
     path('feedback/<int:pk>/details', DetailsFeedbackView.as_view(), name='details-feedback'),
     path('feedback/<int:pk>/edit', EditFeedbackView.as_view(), name='edit-feedback'),
     path('feedback/<int:pk>/delete', DeleteFeedbackView.as_view(), name='delete-feedback'),
-    path('profile/edit', EditUserView.as_view(), name='edit-profile'),
+    path('profile/edit', EditProfileView.as_view(), name='edit-profile'),
+    path('password/', PasswordChangeProfileView.as_view(template_name='dashboard/change-password.html'), name='password'),
+    path('password-success', PasswordSuccess, name='password-success'),
+    
 ]
